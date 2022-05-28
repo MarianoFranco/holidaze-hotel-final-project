f6ce5635a6c5b1ba12213726b7c8b9cd8bc599b9
MarianoFranco <69841893+MarianoFranco@users.noreply.github.com>
2022-05-28 18:42:19 +0200

    location replace

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -306,8 +306,8 @@ function EditHotel({ data, jwt }) {
 			// with router.push the page may be added to history
 			// the browser on history back will  go back to this page and then forward again to the redirected page
 			// you can prevent this behaviour using location.replace
-			Router.push("/login");
-			// location.replace("/login");
+			// Router.push("/login");
+			location.replace("/login");
 		}
 	}, [jwt]);
 
