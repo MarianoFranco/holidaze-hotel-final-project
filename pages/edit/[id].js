5555308020426fed8249a63c811793b641c7c3ad
MarianoFranco <69841893+MarianoFranco@users.noreply.github.com>
2022-05-28 18:33:36 +0200

    test Route

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -297,6 +297,20 @@ function EditHotel({ data, jwt }) {
 	// 	router.push("/login");
 	// }
 	// }, [jwt, router]);
+
+	const [loaded, setLoaded] = useState(false);
+	useEffect(() => {
+		const { pathname } = Router;
+		// conditional redirect
+		if (!jwt) {
+			// with router.push the page may be added to history
+			// the browser on history back will  go back to this page and then forward again to the redirected page
+			// you can prevent this behaviour using location.replace
+			Router.push("/login");
+			// location.replace("/login");
+		}
+	}, [jwt]);
+
 	return (
 		<>
 			<Header user={jwt} />
